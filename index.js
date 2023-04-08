621c488a5d4892e557bfc4aab44d7aa01e8cc4d3
Stella Nthenya <stellaalogi@gmail.com>
2023-04-09 00:10:07 +0300

    Created the four functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Stella Nthenya",
+    streetAddress: "Hum 11\'th street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const secondEmployee = {...employee};
+    delete secondEmployee[key];
+    return secondEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
